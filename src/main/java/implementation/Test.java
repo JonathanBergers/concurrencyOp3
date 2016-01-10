9b7627afb91481bad5184201f9665eae54a83f91
jonathan <jonathan.bergers@gmail.com>
2016-01-10 18:30:29 +0100

    rework

--- src/main/java/implementation/Test.java
+++ src/main/java/implementation/Test.java
@@ -9,6 +9,26 @@ public class Test {
     //TODO testcases
 
 
+    /**
+     *
+     */
+    private static void testCase1(){
+
+        AutoRai a = new AutoRai();
+
+
+
+        for (int i = 0; i < 10; i++) {
+            new Visitor(a).start();
+            new Visitor(a).start();
+            new Buyer(a).start();
+
+        }
+
+
+    }
+
+
 
     public static void main(String[] args) {
 
