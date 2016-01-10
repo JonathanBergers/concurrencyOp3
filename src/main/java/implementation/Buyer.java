aa0102f31a5cdfb61084fa44be42fb95628523d2
Falco <Falcotjeijben@gmail.com>
2016-01-10 18:31:55 +0100

    Merge branch 'jonathan2' of https://github.com/JonathanBergers/concurrencyOp3
    Conflicts:
    	src/main/java/implementation/AccessController.java

--- src/main/java/implementation/Buyer.java
+++ src/main/java/implementation/Buyer.java
@@ -95,7 +95,7 @@ public class Buyer extends AutoRaiFan{
 
     protected void buyCar(){
         //receipt = 100;
-        receipt = (int) (Math.random()*100+1) * 5000;
+        receipt = (int) (Math.random()*100+1) * 500;
         System.out.println(toString() + "just bought a car for "+receipt+" euro's");
     }
 
